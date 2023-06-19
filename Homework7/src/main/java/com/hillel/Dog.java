b670b609004df6fb9776175d342386195923f374
velli <artuuutvelli@gmail.com>
2023-06-19 15:25:11 +0300

    Dog, Animal and Main classes were changed. (Homework8 changed)

--- Homework7/src/main/java/com/hillel/Dog.java
+++ Homework7/src/main/java/com/hillel/Dog.java
@@ -3,22 +3,26 @@ package com.hillel;
 public class Dog {
     private String name;
     private int age;
-    private Animal animal;
+    private Tail tail;
+
+    private String breed;
     private String color;
 
-    public Dog(String name, int age, Animal animal, String color) {
+    public Dog(String name, int age, String breed, Tail tail, String color) {
         this.name = name;
         this.age = age;
-        this.animal = animal;
+        this.breed = breed;
+        this.tail = tail;
         this.color = color;
     }
 
-    public Dog(String name, Animal animal) {
+    public Dog(String name, String breed, Tail tail) {
         this.name = name;
-        this.animal = animal;
+        this.breed = breed;
+        this.tail = tail;
     }
 
     public void printDogCharacteristic() {
-        System.out.println("Dog name: " + name + " ,age: " + age + " , breed: " + animal.printAnimalBreed() + " ,color: " + color);
+        System.out.println("Dog name: " + name + " ,age: " + age + " ,breed: " + breed + " ," + tail.printAnimalProperties() + " ,color: " + color);
     }
 }
--- Homework7/src/main/java/com/hillel/Main.java
+++ Homework7/src/main/java/com/hillel/Main.java
@@ -2,10 +2,10 @@ package com.hillel;
 
 public class Main {
     public static void main(String[] args) {
-        Animal animal1 = new Animal("frenchie");
-        Animal animal2 = new Animal("Jack Rassel");
-        Dog dog1 = new Dog("Dylan", animal1);
-        Dog dog2 = new Dog("Bonnie", 3, animal2, "brown");
+        Tail animal1 = new Tail(false);
+        Tail animal2 = new Tail(true);
+        Dog dog1 = new Dog("Dylan","frenchie",animal1);
+        Dog dog2 = new Dog("Bonnie", 3, "Jack Rassel", animal2, "brown");
 
         dog1.printDogCharacteristic();
         dog2.printDogCharacteristic();
