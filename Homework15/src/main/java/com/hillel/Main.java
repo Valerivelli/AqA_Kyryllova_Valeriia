b0e00dea091e760049ca9d44872d37beb9016345
velli <artuuutvelli@gmail.com>
2023-07-21 13:25:44 +0300

    hw15 done. added exceptions and LoginUtils, main classes

--- Homework15/src/main/java/com/hillel/Main.java
+++ Homework15/src/main/java/com/hillel/Main.java
@@ -7,31 +7,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        boolean isSuccessfulValidation = false;
-
-        System.out.println("Please enter your login:");
-        String login = scanner.nextLine();
-        System.out.println("Please enter your password:");
-        String password = scanner.nextLine();
-        System.out.println("Please confirm your password:");
-        String confirmPassword = scanner.nextLine();
-
-        try {
-            LoginUtils.validateUserLoginData(login, password, confirmPassword);
-            isSuccessfulValidation = true;
-        } catch (WrongLoginException e) {
-            System.out.println(e);
-        } catch (WrongPasswordException ex) {
-            System.out.println(ex);
-        } finally {
-            if (isSuccessfulValidation) {
-                System.out.println(" Successfully validation!");
-            } else System.out.println("Incorrect validation!");
-        }
-
+        LoginUtils.getAndValidateUserInputData();
     }
-
 }
 
 
